9eee4379a553291e7d7c98668e0f9786f2ef23db
ycy <247208124@qq.com>
2023-10-10 19:06:17 +0800

    'deploy'

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git pull -f git@github.com:END888/END888.github.io.git master
+git push -f git@github.com:END888/END888.github.io.git master
+
+cd -
